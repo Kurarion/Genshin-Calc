f1c8195902fcb667cf7db8bbf742c2cf87153580
Sirokuma <sirokuma@irisu.cc>
2022-06-30 20:34:39 +0800

    update enka

--- src/app/shared/component/enka/enka.component.ts
+++ src/app/shared/component/enka/enka.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { TranslateService } from '@ngx-translate/core';
 import { EnkaService } from 'src/app/shared/shared.module';
 
 @Component({
@@ -12,7 +14,7 @@ export class EnkaComponent implements OnInit {
 
   uid = new FormControl();
 
-  constructor(private enkaService: EnkaService, private httpClient: HttpClient) { }
+  constructor(private enkaService: EnkaService, private httpClient: HttpClient, private matSnackBar: MatSnackBar, private translateService: TranslateService) { }
 
   ngOnInit(): void {
     let temp = this.enkaService.getData();
@@ -27,7 +29,21 @@ export class EnkaComponent implements OnInit {
       this.uid.setErrors({'blank': true});
     }else{
       this.uid.setErrors(null);
-      this.enkaService.initEnkaData(this.uid.value);
+      this.enkaService.initEnkaData(this.uid.value).then(()=>{
+        //完了
+        this.translateService.get('ENKA.DONE').subscribe((res: string) => {
+          this.matSnackBar.open(res, undefined, {
+            duration: 1000
+          })
+        });
+      }).catch(()=>{
+        //失敗
+        this.translateService.get('ENKA.FAILED').subscribe((res: string) => {
+          this.matSnackBar.open(res, undefined, {
+            duration: 1000
+          })
+        });
+      })
     }
   }
 
